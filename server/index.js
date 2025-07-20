155bf3ab03851fb50fdf58fa1f7ebd2f04b7df33
Davalorlk <marcbelem55@gmail.com>
2025-07-20 17:00:48 +0100

    feat: Add global request logger to server
    Added a middleware to log all incoming requests to the server for debugging purposes.

--- server/index.js
+++ server/index.js
@@ -45,6 +45,12 @@ const pool = new Pool({
 
 app.use(express.json());
 
+// Global request logger
+app.use((req, res, next) => {
+  console.log(`[Request] ${req.method} ${req.url}`);
+  next();
+});
+
 // Example REST endpoint
 dbTest = async (req, res) => {
   try {
